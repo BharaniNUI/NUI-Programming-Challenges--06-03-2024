cf17a13c9c4bd24da6ab650339051d94f5750e09
BharaniNUI <bharanidaran.g@numentica-ui.com>
2024-03-07 15:51:50 +0530

    second commit

--- 1.FormatPhoneNum.js
+++ 1.FormatPhoneNum.js
@@ -1,26 +1,31 @@
 function phoneFormat(phoneNumber) {
-    phoneNumber = String(phoneNumber); //Phonenumber converted to string
+    if (typeof phoneNumber === 'number') {
+        phoneNumber = String(phoneNumber); //Phonenumber converted to string
 
-    if (phoneNumber.length === 10) { //Condition to check phonenumber with 10 digit
+        if (phoneNumber.length === 10) { //Condition to check phonenumber with 10 digit
 
-        resultphoneNumber = "("; //Adding "(" initially
-        let stringphoneNumber = "";
+            resultphoneNumber = "("; //Adding "(" initially
+            let stringphoneNumber = "";
 
-        for (let i = 0; i < phoneNumber.length; i++) {
+            for (let i = 0; i < phoneNumber.length; i++) {
 
-            stringphoneNumber += phoneNumber.charAt(i);
-            if (i === 2) {
-                stringphoneNumber += ")"; //Adding ")" after three digits
+                stringphoneNumber += phoneNumber.charAt(i);
+                if (i === 2) {
+                    stringphoneNumber += ")"; //Adding ")" after three digits
+                }
+                else if (i === 5) {
+                    stringphoneNumber += "-"; //Adding "-" after six digits
+                }
+
             }
-            else if (i === 5) {
-                stringphoneNumber += "-"; //Adding "-" after six digits
-            }
-
+            return resultphoneNumber += stringphoneNumber; //Concat with initial value
+        } else {
+            return `Invalid PhoneNumber! Enter 10 digit PhoneNumber`;
         }
-        return resultphoneNumber += stringphoneNumber; //Concat with initial value
     } else {
-        return `Invalid PhoneNumber! Enter 10 digit PhoneNumber`;
+        return `Invalid PhoneNumber! Enter PhoneNumber in Number Format`;
     }
+
 }
 
 console.log(phoneFormat(9840164723))
